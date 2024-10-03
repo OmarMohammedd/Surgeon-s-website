3fee4657e8f0b563ff9bffea394e55068b24e8ab
Omar Mohamed <99252081+OmarMohammedd@users.noreply.github.com>
2024-10-03 14:25:12 +0300

    update

--- assets/js/email.js
+++ assets/js/email.js
@@ -3,7 +3,8 @@ function sendMail() {
 	var params = {
 	  name: document.getElementById("name").value,
 	  phone: document.getElementById("phone").value,
-	  location: document.getElementById("location").value,
+	  clinic: document.getElementById("clinic").value,
+	  appointment: document.getElementById("appointment").value,
 	};
   
 	  emailjs.send("service_fo4gk5a", "template_ehs6mh3", params).then(alert("تم الارسال بنجاح"))
